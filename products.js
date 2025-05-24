61a4b56d18dc64aa8e255e373825cae801c82e29
gadget-cave <Laphixzam@gmail.com>
2025-05-24 22:37:03 +0530

    Update products.js

--- products.js
+++ products.js
@@ -1,168 +1,178 @@
-const products = [
-  {
-    name: "Wireless Earphones-CMF BY NOTHING",
-    image: "https://i.ibb.co/cXhm2q40/earphones.jpg",
-    description: "High-quality sound and battery life.",
-    price: "₹899",
-    longDescription:"Enjoy crisp and clear sound with deep bass. Battery lasts up to 10 hours per charge.",
-    extraImages:[]
-  },
-  {
-    name: "HEADPHONES-P9",
-    image: "https://i.ibb.co/vCJrF2NZ/headphonesp9.jpg",
-    description: "High quality, C-type charging",
-    price: "₹699",
-    longDescription:"",
-    extraImages:[]
-  },
-  {
-    name: "Samsung Watch with Airpods",
-    image: "https://i.ibb.co/S4KZqPkG/samsung-watch-with-airpods.jpg",
-    description: "Fitness tracker and call support.",
-    price: "₹1199",
-    longDescription:"",
-    extraImages:[]
-  },
-  {
-    name: "CASIO G-SHOCK",
-    image: "https://i.ibb.co/9HTJwrjN/CASIO-G-SHOCK.jpg",
-    description: "Awesome quality, chain model.",
-    price: "₹999",
-    longDescription:"",
-    extraImages:[]
-  },
-   {
-    name: "TRACKON - watch",
-    image: "https://i.ibb.co/sp6nVFHD/TRACKON-watch.jpg",
-    description: "Awesome quality,trending model.",
-    price: "₹599",
-    longDescription:"",
-    extraImages:[]
-  },
-   {
-    name: "Marshal Ember ANC - earpods",
-    image: "https://i.ibb.co/8nfGR3vk/Marshal-Ember-ANC-earpod.jpg",
-    description: "High-quality sound,6 eartips,1 charging cable.",
-    price: "₹999",
-    longDescription:"",
-    extraImages:[]
-  },
-   {
-    name: "MZ bluetooth speaker",
-    image: "https://i.ibb.co/gZYPxgYS/MZ-bluetooth-speaker.jpg",
-    description: "High-quality sound and trending model.",
-    price: "₹699",
-    longDescription:"",
-    extraImages:[]
-  },
-  {
-    name: "HTC-trimmer",
-    image: "https://i.ibb.co/dwydCFJZ/HTC-trimmer.jpg",
-    description: "Awesome quality,latest arrivals.",
-    price: "₹649",
-    longDescription:"",
-    extraImages:[]
-  },
-   {
-    name: "Magnet locking watch",
-    image: "https://i.ibb.co/CKMPZjDG/Magnet-locking-watch.jpg",
-    description: "click more details☝️.",
-    price: "₹749",
-    longDescription:"",
-    extraImages:[
-      "https://i.ibb.co/QvL7gvLd/magnet-locking-watch-brown.jpg",
-      "https://i.ibb.co/9Hb8fRMw/magnet-locking-watch-green.jpg"
-    ]
-  },
-   {
-    name: "MARSHALL headset",
-    image: "https://i.ibb.co/9Dxz1Xq/MARSHALL-headset.jpg",
-    description: "High-quality sound and latest arrivals.",
-    price: "₹949",
-    longDescription:"",
-    extraImages:[]
-  },
-   {
-    name: "BOAT AIRBUDS",
-    image: "https://i.ibb.co/RkSWyDKh/BOAT-airbuds.jpg",
-    description: "click more details☝️.",
-    price: "₹749",
-    longDescription:"500 mah battery,wireless earpods, call time upto 5hr",
-    extraImages:[
-      "https://i.ibb.co/CSrCsWT/BOAT-airbuds-blue.jpg",
-      "https://i.ibb.co/RpG2JZZR/BOAT-airbuds-mix.jpg"
-    ]
-  },
-   {
-    name: "best ever combo",
-    image: "https://i.ibb.co/21yghLzS/apple-watch-headset.jpg",
-    description: "click more details☝️.",
-    price: "₹1499",
-    longDescription:"Apple watch + P9 headset + airpods 2 + Rendom bag",
-    extraImages:[
-      "https://i.ibb.co/8Ls04V5q/apple-watch-combo-black.jpg"
-    ]
-  },
-   {
-    name: "WATCH- LG GOLD",
-    image: "https://i.ibb.co/KjxM14sD/LG-GOLD-watch.jpg",
-    description: "click more details☝️.",
-    price: "₹549",
-    longDescription:"Awesome quality, latest arrivals",
-    extraImages:[
-      "https://i.ibb.co/sdkX47Kz/LG-GOLD-green.jpg"
-    ]
-  },
-   {
-    name: "BOAT Airdopes 600",
-    image: "https://i.ibb.co/JjvhcFZt/BOAT-airdopes-600.jpg",
-    description: "click more detils☝️. ",
-    price: "₹749",
-    longDescription:"Wireless earphone,500 mah battery,call time upto 5hr",
-    extraImages:[
-      "https://i.ibb.co/5xT1pQ4R/BOAT-airdopes-600-black.jpg",
-      "https://i.ibb.co/vxJsdnfB/BOAT-airdopes-600-blue.jpg"
-    ]
-  },
-   {
-    name: "Samsung GALAXY BUDS 2 PRO",
-    image: "https://i.ibb.co/j9thw23H/Samsung-GALAXY-BUDS-2-PRO.jpg",
-    description: "click more details☝️.",
-    price: "₹849",
-    longDescription: "touch operation,type-c charging,Battery Capacity 700mAh,Ceramic Finish premium Earbuds Design,Next Generation PixArt Chipset For Lossless Connectivity.",
-    extraImages: [
-      "https://i.ibb.co/ksq6jSHQ/galaxy-buds-white.jpg",
-      "https://i.ibb.co/Z63zTgRN/galaxy-buds-black.jpg"  // example extra image
-    ]
-  }, 
-   {
-    name: "Ultra smart watch special edition",
-    image: "https://i.ibb.co/xq4Tr7FS/Ultra-smart-watch-special-edition.jpg",
-    description: "click more details☝️",
-    price: "₹749",
-    longDescription:"5+menu style,fitpro app,wireless charging,etc..",
-    extraImages:[
-      "https://i.ibb.co/qLk2hhLv/ultra.jpg"
-    ]
-  },
-   {
-    name: "MASSAGE GUN",
-    image: "https://i.ibb.co/fYbbQTbc/MASSAGE-GUN.jpg",
-    description: "Massage Gun | Strike Handheld Percussion Massage Gun with Interchangeable Massage Heads",
-    price: "₹749",
-    longDescription:"",
-    extraImages:[]
-  },
-   {
-     name: "premium airpods pro2",
-    image: "https://i.ibb.co/LhZRNFQf/apple-airpod-pro2-white.jpg",
-    description: "click more details☝️",
-    price: "₹799",
-    longDescription:"5-8 hours battery backup including case,Premium build material,Compatible with all devices,Both Sensors Fully Functional ,598mah battery,Premium Metal Engraving ,Extra buds ",
-    extraImages:[
-      "https://i.ibb.co/XxD8StC8/apple-airpod-pro2-black.jpg",
-      "https://i.ibb.co/Xrv0n01C/apple-airpod-pro-2-mix.jpg"
-    ]
-  }
-];
+const productsByCategory = {
+  "Earbuds": [
+    {
+      name: "Wireless Earphones-CMF BY NOTHING",
+      image: "https://i.ibb.co/cXhm2q40/earphones.jpg",
+      description: "High-quality sound and battery life.",
+      price: "₹899",
+      longDescription: "Enjoy crisp and clear sound with deep bass. Battery lasts up to 10 hours per charge.",
+      extraImages: []
+    },
+    {
+      name: "BOAT AIRBUDS",
+      image: "https://i.ibb.co/RkSWyDKh/BOAT-airbuds.jpg",
+      description: "click more details☝️.",
+      price: "₹749",
+      longDescription:"500 mah battery,wireless earpods, call time upto 5hr",
+      extraImages:[
+        "https://i.ibb.co/CSrCsWT/BOAT-airbuds-blue.jpg",
+        "https://i.ibb.co/RpG2JZZR/BOAT-airbuds-mix.jpg"
+      ]
+    },
+    {
+      name: "BOAT Airdopes 600",
+      image: "https://i.ibb.co/JjvhcFZt/BOAT-airdopes-600.jpg",
+      description: "click more detils☝️.",
+      price: "₹749",
+      longDescription:"Wireless earphone,500 mah battery,call time upto 5hr",
+      extraImages:[
+        "https://i.ibb.co/5xT1pQ4R/BOAT-airdopes-600-black.jpg",
+        "https://i.ibb.co/vxJsdnfB/BOAT-airdopes-600-blue.jpg"
+      ]
+    },
+    {
+      name: "Samsung GALAXY BUDS 2 PRO",
+      image: "https://i.ibb.co/j9thw23H/Samsung-GALAXY-BUDS-2-PRO.jpg",
+      description: "click more details☝️.",
+      price: "₹849",
+      longDescription: "touch operation,type-c charging,Battery Capacity 700mAh,Ceramic Finish premium Earbuds Design,Next Generation PixArt Chipset For Lossless Connectivity.",
+      extraImages: [
+        "https://i.ibb.co/ksq6jSHQ/galaxy-buds-white.jpg",
+        "https://i.ibb.co/Z63zTgRN/galaxy-buds-black.jpg"
+      ]
+    },
+    {
+      name: "premium airpods pro2",
+      image: "https://i.ibb.co/LhZRNFQf/apple-airpod-pro2-white.jpg",
+      description: "click more details☝️",
+      price: "₹799",
+      longDescription:"5-8 hours battery backup including case,Premium build material,Compatible with all devices,Both Sensors Fully Functional ,598mah battery,Premium Metal Engraving ,Extra buds ",
+      extraImages:[
+        "https://i.ibb.co/XxD8StC8/apple-airpod-pro2-black.jpg",
+        "https://i.ibb.co/Xrv0n01C/apple-airpod-pro-2-mix.jpg"
+      ]
+    }
+  ],
+  "Headphones & Headsets": [
+    {
+      name: "HEADPHONES-P9",
+      image: "https://i.ibb.co/vCJrF2NZ/headphonesp9.jpg",
+      description: "High quality, C-type charging",
+      price: "₹699",
+      longDescription: "",
+      extraImages: []
+    },
+    {
+      name: "MARSHALL headset",
+      image: "https://i.ibb.co/9Dxz1Xq/MARSHALL-headset.jpg",
+      description: "High-quality sound and latest arrivals.",
+      price: "₹949",
+      longDescription: "",
+      extraImages: []
+    },
+    {
+      name: "Marshal Ember ANC - earpods",
+      image: "https://i.ibb.co/8nfGR3vk/Marshal-Ember-ANC-earpod.jpg",
+      description: "High-quality sound,6 eartips,1 charging cable.",
+      price: "₹999",
+      longDescription: "",
+      extraImages: []
+    }
+  ],
+  "Smart Watches": [
+    {
+      name: "Samsung Watch with Airpods",
+      image: "https://i.ibb.co/S4KZqPkG/samsung-watch-with-airpods.jpg",
+      description: "Fitness tracker and call support.",
+      price: "₹1199",
+      longDescription: "",
+      extraImages: []
+    },
+    {
+      name: "Ultra smart watch special edition",
+      image: "https://i.ibb.co/xq4Tr7FS/Ultra-smart-watch-special-edition.jpg",
+      description: "click more details☝️",
+      price: "₹749",
+      longDescription:"5+menu style,fitpro app,wireless charging,etc..",
+      extraImages:[
+        "https://i.ibb.co/qLk2hhLv/ultra.jpg"
+      ]
+    },
+    {
+      name: "WATCH- LG GOLD",
+      image: "https://i.ibb.co/KjxM14sD/LG-GOLD-watch.jpg",
+      description: "click more details☝️.",
+      price: "₹549",
+      longDescription:"Awesome quality, latest arrivals",
+      extraImages:[
+        "https://i.ibb.co/sdkX47Kz/LG-GOLD-green.jpg"
+      ]
+    }
+  ],
+  "Speakers & Accessories": [
+    {
+      name: "MZ bluetooth speaker",
+      image: "https://i.ibb.co/gZYPxgYS/MZ-bluetooth-speaker.jpg",
+      description: "High-quality sound and trending model.",
+      price: "₹699",
+      longDescription: "",
+      extraImages: []
+    },
+    {
+      name: "HTC-trimmer",
+      image: "https://i.ibb.co/dwydCFJZ/HTC-trimmer.jpg",
+      description: "Awesome quality,latest arrivals.",
+      price: "₹649",
+      longDescription: "",
+      extraImages: []
+    },
+    {
+      name: "MASSAGE GUN",
+      image: "https://i.ibb.co/fYbbQTbc/MASSAGE-GUN.jpg",
+      description: "Massage Gun | Strike Handheld Percussion Massage Gun with Interchangeable Massage Heads",
+      price: "₹749",
+      longDescription: "",
+      extraImages: []
+    }
+  ],
+  "Combos & Offers": [
+    {
+      name: "best ever combo",
+      image: "https://i.ibb.co/21yghLzS/apple-watch-headset.jpg",
+      description: "click more details☝️.",
+      price: "₹1499",
+      longDescription:"Apple watch + P9 headset + airpods 2 + Rendom bag",
+      extraImages:[
+        "https://i.ibb.co/8Ls04V5q/apple-watch-combo-black.jpg"
+      ]
+    },
+    {
+      name: "Magnet locking watch",
+      image: "https://i.ibb.co/CKMPZjDG/Magnet-locking-watch.jpg",
+      description: "click more details☝️.",
+      price: "₹749",
+      longDescription:"",
+      extraImages:[
+        "https://i.ibb.co/QvL7gvLd/magnet-locking-watch-brown.jpg",
+        "https://i.ibb.co/9Hb8fRMw/magnet-locking-watch-green.jpg"
+      ]
+    },
+    {
+      name: "TRACKON - watch",
+      image: "https://i.ibb.co/sp6nVFHD/TRACKON-watch.jpg",
+      description: "Awesome quality,trending model.",
+      price: "₹599",
+      longDescription:"",
+      extraImages: []
+    },
+    {
+      name: "CASIO G-SHOCK",
+      image: "https://i.ibb.co/9HTJwrjN/CASIO-G-SHOCK.jpg",
+      description: "Awesome quality, chain model.",
+      price: "₹999",
+      longDescription:"",
+      extraImages: []
+    }
+  ]
+};
 
